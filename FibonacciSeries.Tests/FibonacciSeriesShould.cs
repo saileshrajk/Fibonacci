e4fb9d3120412c14b8b1ca844fca700376e20f62
saileshrajk <saileshrajk@gmail.com>
2016-03-30 11:40:57 +0100

    added implementation for testing zero as first output

---
+++ FibonacciSeries.Tests/FibonacciSeriesShould.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FibonacciSeries.Tests
+{
+    
+    [TestFixture]
+    public class FibonacciSeriesShould
+    {
+        [Test]
+        public void return_a_list_containing_zero()
+        {
+            var fibonacciSeries = new FibonacciSeries();
+
+            var fibonacciList = fibonacciSeries.GetList();
+            
+            Assert.That(new List<int>{0}, Is.EqualTo(fibonacciList));
+        }
+    }
+}
