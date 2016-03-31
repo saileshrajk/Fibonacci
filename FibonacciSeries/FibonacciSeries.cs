434af892ba1c1fbd8b943a3283e5fcf4066346f0
saileshrajk <saileshrajk@gmail.com>
2016-03-31 14:17:31 +0100

    refactored code to implement for next

--- FibonacciSeries/FibonacciSeries.cs
+++ FibonacciSeries/FibonacciSeries.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace FibonacciSeries
 {
@@ -10,6 +11,31 @@ namespace FibonacciSeries
             {
                 return new List<int> {0};
             }
+
+            var fibonaccinumbers = new List<int>();
+            for (int i = 0; i < length; i++)
+            {
+                //give me the last index
+                //retrieve last element
+                // curr = last + secondLast
+
+                if (i == 0 || i== 1)
+                {
+                    fibonaccinumbers.Add(i);
+                    continue;
+                }
+
+                var lastIndex = fibonaccinumbers.Count -1;
+
+                var secondLastIndex = lastIndex - 1;
+
+                var currentFibonaccinumber = fibonaccinumbers[lastIndex] + fibonaccinumbers[secondLastIndex];
+
+                fibonaccinumbers.Add(currentFibonaccinumber);
+            }
+
+            return fibonaccinumbers;
+
             if (length == 2)
             {
                 return new List<int> {0, 1};
